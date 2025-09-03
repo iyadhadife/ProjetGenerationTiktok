6ce4fa9c58f5488ae826cb631f62043ef7112858
iyadhadife <ihadife@gmail.com>
2025-09-03 16:08:57 +0200

    Adding color choice

--- Wall.py
+++ Wall.py
@@ -4,9 +4,10 @@ from Ball import Ball
 
 class Wall:
     """Classe générique pour un mur ou une frontière."""
-    def __init__(self, x, y):
+    def __init__(self, x, y,color):
         self.x = x
         self.y = y
+        self.color = color
 
     def distance_to_middle(self,  ball : Ball):
         """Calculer la distance à la balle (à surcharger si nécessaire)."""
@@ -25,12 +26,13 @@ class Wall:
         raise NotImplementedError("Cette méthode doit être surchargée dans la sous-classe.")
 
 class CircleWall(Wall):
-    def __init__(self, x, y, radius, start_angle, end_angle, id):
+    def __init__(self, x, y, radius, start_angle, end_angle, id,color):
         self.x = x
         self.y = y
         self.radius = radius
         self.start_angle = 0
         self.end_angle = math.pi*2
+        self.color = color
         self.id = id
 
     def draw(self, surface):
@@ -58,17 +60,18 @@ class CircleWall(Wall):
         return math.pi * (self.radius ** 2)
 
 class ArcWall(Wall):
-    def __init__(self, x, y, radius, start_angle, end_angle, id):
+    def __init__(self, x, y, radius, start_angle, end_angle, id,color):
         self.x = x
         self.y = y
         self.radius = radius
         self.start_angle = start_angle
         self.end_angle = end_angle
+        self.color = color
         self.id = id
 
     def draw(self, surface):
         rect = pygame.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
-        pygame.draw.arc(surface, (0, 100, 255), rect, self.start_angle, self.end_angle, 5)
+        pygame.draw.arc(surface, self.color, rect, self.start_angle, self.end_angle, 5)
 
     def distance_to_middle(self, ball : Ball):
         dx = ball.x - self.x
